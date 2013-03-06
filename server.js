0d6c9afd6108fc2974bf2e180636a4baada2e151
Matthew McCants <mattmccants@gmail.com>
2013-03-06 02:59:57 -0600

    Just testing socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+var io = require('socket.io').listen(9000)
+
+var users = [];
+
+io.sockets.on('connection', function (socket) {
+	socket.on('join', function(username, callback) {
+		if (users.indexOf(username) < 0) {
+			socket.username = username;
+			users.push(username);
+			socket.broadcast.emit('user-joined', username);
+			callback(true, users);
+		} else {
+			callback(false);
+		}
+	});
+
+	socket.on("chat", function(message) {
+		if (socket.username && message) {
+			io.sockets.emit("chat", {sender: socket.username, message: message});
+		}
+	});
+
+	socket.on('disconnect', function() {
+		if (socket.username) {
+			users.splice(users.indexOf(socket.username), 1);
+			io.sockets.emit('user-left', socket.username);
+		}
+	});
+});
