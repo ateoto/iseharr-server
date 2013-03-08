396b2483711be55c0de3ba901bda64b6a9c98c86
Matthew McCants <mattmccants@gmail.com>
2013-03-08 13:24:10 -0600

    Less verbose, more informative.

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ io.sockets.on('connection', function (socket) {
 			socket.pc = new pc.PlayerCharacter(socket.username);
 			users.push(socket.pc);
 			socket.broadcast.emit('user-joined', { pc: socket.pc });
+			console.log('User Joined: ' + socket.pc.username);
 			callback(true, users);
 		} else {
 			callback(false);
@@ -32,7 +33,6 @@ io.sockets.on('connection', function (socket) {
 	socket.on('pc-move', function(coordinates) {
 		if (socket.pc && coordinates) {
 			//Sanitize coordinate input
-			console.log(socket.pc);
 			socket.pc.position.x = coordinates.x;
 			socket.pc.position.y = coordinates.y;
 			io.sockets.emit("pc-move-ack", { pc: socket.pc });
@@ -48,6 +48,7 @@ io.sockets.on('connection', function (socket) {
 	socket.on('disconnect', function() {
 		if (socket.username) {
 			users.splice(users.indexOf(socket.pc), 1);
+			console.log('User left: ' + socket.pc.username);
 			io.sockets.emit('user-left', {pc: socket.pc});
 		}
 	});
