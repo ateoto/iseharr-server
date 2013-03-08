8ce2ae56cc51578e10284b4d4175a826bb329269
Matthew McCants <mattmccants@gmail.com>
2013-03-08 12:15:56 -0600

    Basic moving code.

--- server.js
+++ server.js
@@ -6,15 +6,36 @@ io.set('log level', 1);
 
 var users = [];
 
+var last_tick = Date.now();
+
+tick = function() {
+	var current_tick = Date.now();
+	// Currently, nothing.
+	last_tick = current_tick;
+}
+
+setInterval(tick, 10);
+
 io.sockets.on('connection', function (socket) {
 	socket.on('join', function(username, callback) {
 		if (users.indexOf(username) < 0) {
 			socket.username = username;
-			users.push(username);
-			socket.broadcast.emit('user-joined', username);
+			socket.pc = new pc.PlayerCharacter(socket.username);
+			users.push(socket.pc);
+			socket.broadcast.emit('user-joined', { pc: socket.pc });
 			callback(true, users);
 		} else {
 			callback(false);
+		}
+	});
+
+	socket.on('pc-move', function(coordinates) {
+		if (socket.pc && coordinates) {
+			//Sanitize coordinate input
+			console.log(socket.pc);
+			socket.pc.position.x = coordinates.x;
+			socket.pc.position.y = coordinates.y;
+			io.sockets.emit("pc-move-ack", { pc: socket.pc });
 		}
 	});
 
@@ -26,8 +47,8 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('disconnect', function() {
 		if (socket.username) {
-			users.splice(users.indexOf(socket.username), 1);
-			io.sockets.emit('user-left', socket.username);
+			users.splice(users.indexOf(socket.pc), 1);
+			io.sockets.emit('user-left', {pc: socket.pc});
 		}
 	});
 });
