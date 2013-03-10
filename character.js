8d1d7da79a508200bae338c491036f00ac4263bf
Matthew McCants <mattmccants@gmail.com>
2013-03-09 19:02:27 -0600

    Namespacing was the issue.

--- character.js
+++ character.js
@@ -3,5 +3,13 @@ var point = require('./point.js');
 exports.PlayerCharacter = function(username) {
 	this.username = username;
 	this.health = 100;
+	this.is_moving = false;
+	this.facing =  new point.Point(0,-1);
 	this.position = new point.Point(576,280);
 }
+
+
+// North ( 0,  1)
+// South ( 0, -1)
+// East  ( 1,  0)
+// West  (-1,  0)
--- server.js
+++ server.js
@@ -1,55 +1,55 @@
 var io = require('socket.io').listen(8080);
 var pc = require('./character.js');
+var bison = require('bison');
 
 
-io.set('log level', 1);
+//io.set('log level', 1);
 
 var users = [];
 
 var last_tick = Date.now();
 
 tick = function() {
-	var current_tick = Date.now();
-	// Currently, nothing.
-	last_tick = current_tick;
+    var current_tick = Date.now();
+    // Currently, nothing.
+    last_tick = current_tick;
 }
 
 setInterval(tick, 10);
 
-io.sockets.on('connection', function (socket) {
-	socket.on('join', function(username, callback) {
-		if (users.indexOf(username) < 0) {
-			socket.username = username;
-			socket.pc = new pc.PlayerCharacter(socket.username);
-			users.push(socket.pc);
-			socket.broadcast.emit('user-joined', { pc: socket.pc });
-			console.log('User Joined: ' + socket.pc.username);
-			callback(true, users);
-		} else {
-			callback(false);
-		}
-	});
+var main_socket = io.of('/socket').on('connection', function(socket) {
+    socket.on('join', function(username, callback) {
+        if (users.indexOf(username) < 0) {
+            socket.username = username;
+            socket.pc = new pc.PlayerCharacter(socket.username);
+            users.push(socket.pc);
+            socket.broadcast.emit('user-joined', { pc: socket.pc });
+            console.log('User Joined: ' + socket.pc.username);
+            callback(true, users);
+        } else {
+            callback(false);
+        }
+    });
 
-	socket.on('pc-move', function(coordinates) {
-		if (socket.pc && coordinates) {
-			//Sanitize coordinate input
-			socket.pc.position.x = coordinates.x;
-			socket.pc.position.y = coordinates.y;
-			io.sockets.emit("pc-move-ack", { pc: socket.pc });
-		}
-	});
+    socket.on('pc-move', function(coordinates) {
+        if (socket.pc && coordinates) {
+            socket.pc.position.x = coordinates.x;
+            socket.pc.position.y = coordinates.y;
+            main_socket.emit('pc-move-ack', { pc: socket.pc });
+        }
+    });
 
-	socket.on("chat", function(message) {
-		if (socket.username && message) {
-			io.sockets.emit("chat", {sender: socket.username, message: message});
-		}
-	});
+    socket.on('chat', function(message) {
+        if (socket.username && message) {
+            socket.broadcast.emit('chat', { sender: socket.username, message: message });
+        }
+    });
 
-	socket.on('disconnect', function() {
-		if (socket.username) {
-			users.splice(users.indexOf(socket.pc), 1);
-			console.log('User left: ' + socket.pc.username);
-			io.sockets.emit('user-left', {pc: socket.pc});
-		}
-	});
+    socket.on('disconnect', function() {
+        if (socket.username) {
+            users.splice(users.indexOf(socket.pc), 1);
+            console.log('User left: ' + socket.pc.username);
+            main_socket.emit('user-left', { pc: socket.pc });
+        }
+    });
 });
